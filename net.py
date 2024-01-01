735e106443623b1b75e3905cb2ab44896009266c
Tomohiro Oga <144699781+tomooga9@users.noreply.github.com>
2024-01-01 17:21:08 -0500

    Updated Files

--- net.py
+++ net.py
@@ -1,5 +1,7 @@
 import torch
-import torch.nn
+import torch.nn as nn
+from synthesis import SynthesisNet
+from refinement import RefinementNet
 
 class Net(nn.Module):
     def __init__(self):
@@ -13,14 +15,3 @@ class Net(nn.Module):
         rough_frame = x
         refined_frame = self.refinement(x, I_2, I_3)
         return rough_frame, refined_frame
-
-def loss_fn(synthesis_frame, refinement_frame, ground_truth):
-    l1_loss = nn.L1Loss()
-    L_1 = l1_loss(synthesis_frame, ground_truth)
-    L_2 = l1_loss(refinement_frame, ground_truth)
-    L = 0.5 * L_1 + L_2
-    return L
-
-def get_optimizer(model):
-    return nn.Adam(model.parameters(), lr=1e-4)
-    
